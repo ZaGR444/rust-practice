876c8ad2516daf158b8726da2f27b63e447d6649
ZaGR444 <nikita.zagorodny@e-u.edu.ua>
2025-03-17 13:22:53 +0100

    Update hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,32 @@
+  ub fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().to_string()
+            } else {
+                c.to_lowercase().to_string()
+            }
+        })
+        .collect::<String>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let data = [
+            ("Hello", "hELLO"),
+            ("Привет", "пРИВЕТ"),
+            ("Rust", "rUST"),
+            ("Test123", "tEST123"),
+            ("UPPER lower", "upper LOWER"),
+        ];
+
+        data.iter().for_each(|(a, b)| {
+            assert_eq!(invert_the_case(a.to_string()), b.to_string());
+            assert_eq!(invert_the_case(b.to_string()), a.to_string());
+        });
+    }
+}
